b586c1e88b97b75ee2f5f1732ee571d13d7e97f7
Radehshyam Lodhi <radheshyamlodhi48@gmail.com>
2024-06-29 01:36:38 +0530

    isActive  using isactive change nave css

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function More() {
   return (
-    <div>More</div>
+    <div className='w-1/2 text-center border-2 border-zinc-600 translate-x-1/2 mt-5 bg-green-100'>
+      <h1 className='text-3xl font-bolder text-blue-400 capitalize mb-5'>more</h1>
+      <p className='text-xl font-light p-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur perferendis fugit earum aperiam voluptatibus sed ut assumenda culpa vero laborum?</p>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-
 function Nav() {
   return (
     <>
-      <nav className="flex justify-center items-center bg-zinc-300 font-semibold">
+      <nav className="flex justify-center items-center bg-zinc-300 font-semibold p-3 text-2xl">
         <ul className="flex gap-10 ">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink style={(e)=>{
+              return {
+                color:e.isActive ? "red" : "",
+                fontWeight: e.isActive?"bolder":""
+              }
+            }} to="/">
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/more">more</Link>
+            <NavLink to="/more">
+            {(e)=>{
+              return  <span  className={[e.isActive ? "text-red-500":"",
+                 e.isActive?"font-bolder":""
+
+              ].join(" ")}>more</span>
+              
+            }}
+            </NavLink>
           </li>
           <li>
-            <Link to="/about">about</Link>
+            <NavLink className={(e)=>{
+             
+              return  [e.isActive?"text-red-500":"",e.isActive?"font-boler":""].join(" ");
+              
+            }} to="/about">about</NavLink>
           </li>
+          
         </ul>
       </nav>
     </>
